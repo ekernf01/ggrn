008096767196d92f7d7ea9ce04a2cca910dca74d
Eric Kernfeld <eric.kern13@gmail.com>
2024-03-14 13:44:07 -0400

    Get prescient running end to end in a container

--- ggrn_docker_backend/Dockerfiles/prescient/build.sh
+++ ggrn_docker_backend/Dockerfiles/prescient/build.sh
@@ -4,8 +4,8 @@
 docker build -t ggrn_docker_backend_prescient .
 
 # We use this for easy interactive development but it should usually be commented out. 
-docker run --rm -it -v /home/ekernf01/Desktop/jhu/research/projects/perturbation_prediction/cell_type_knowledge_transfer/ggrn/ggrn_docker_backend/from_to_docker:/from_to_docker ggrn_docker_backend_prescient
+# docker run --rm -it -v /home/ekernf01/Desktop/jhu/research/projects/perturbation_prediction/cell_type_knowledge_transfer/ggrn/ggrn_docker_backend/from_to_docker:/from_to_docker ggrn_docker_backend_prescient
 
-# docker tag ggrn_docker_backend_prescient ekernf01/ggrn_docker_backend_prescient
-# docker login
-# docker push ekernf01/ggrn_docker_backend_prescient
+docker tag ggrn_docker_backend_prescient ekernf01/ggrn_docker_backend_prescient
+docker login
+docker push ekernf01/ggrn_docker_backend_prescient

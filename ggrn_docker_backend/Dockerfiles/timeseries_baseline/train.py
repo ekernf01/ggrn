47687d941f35962a37deb8d087d15e90c265cef4
Eric Kernfeld <eric.kern13@gmail.com>
2024-06-04 11:39:47 -0400

    Average 100 simulated cells by default in the timeseries baseline

--- ggrn_docker_backend/Dockerfiles/timeseries_baseline/train.py
+++ ggrn_docker_backend/Dockerfiles/timeseries_baseline/train.py
@@ -8,9 +8,6 @@ import sys
 sys.path.append(".")
 
 train = sc.read_h5ad("from_to_docker/train.h5ad")
-nonzero_columns=np.where(train.X.sum(axis=0) != 0)[1]
-train = train[:,nonzero_columns]
-train.layers['norm_counts'] = train.X.copy()
 
 predictions_metadata = pd.read_csv("from_to_docker/predictions_metadata.csv")
 for c in ['timepoint', 'cell_type', 'perturbation', "expression_level_after_perturbation", 'prediction_timescale']:
@@ -24,6 +21,14 @@ with open(os.path.join("from_to_docker/kwargs.json")) as f:
 
 with open(os.path.join("from_to_docker/ggrn_args.json")) as f:
     ggrn_args = json.load(f)
+
+
+defaults = {
+    "num_cells_to_simulate": 100,
+}
+for k in defaults:
+    if k not in kwargs:
+        kwargs[k] = defaults[k]
 
 def predict_one(tf: str, expression_level_after_perturbation: float, cell_type: str, timepoint: int, num_steps: int):
     """Predict expression after TF perturbation
@@ -39,15 +44,25 @@ def predict_one(tf: str, expression_level_after_perturbation: float, cell_type: 
     current_cell = np.random.choice(current_cell, size = 1)
     for _ in range(num_steps):
         # Find descendents and select one at random
-        current_cell = [c for c in train.obs_names if train.obs.loc[c, "matched_control"] in current_cell]
-        if len(current_cell) == 0:
-            raise ValueError("No descendents found at the indicated timepoint.")
-        current_cell = np.random.choice(current_cell, size = 1)
+        next_cells = [c for c in train.obs_names if train.obs.loc[c, "matched_control"] in current_cell]
+        if len(next_cells) == 0:
+            return np.nan
+        current_cell = np.random.choice(next_cells, size = 1)
     try:
-        return train[current_cell, :].X.toarray()
+        return train[current_cell, :].X.toarray().mean(axis=0)
     except AttributeError:
-        return train[current_cell, :].X
+        return train[current_cell, :].X.mean(axis=0)
     
+def predict_many(num_cells_to_simulate: int, tf: str, expression_level_after_perturbation: float, cell_type: str, timepoint: int, num_steps: int):
+    average_prediction = np.zeros(train.n_vars)
+    for j in range(num_cells_to_simulate):
+        a_single_prediction = predict_one(tf, expression_level_after_perturbation, cell_type, timepoint, num_steps)
+        if any(pd.isnull(a_single_prediction)):
+            num_cells_to_simulate -= 1
+        else:
+            average_prediction += a_single_prediction
+    return average_prediction / num_cells_to_simulate
+
 print("Running simulations")
 predictions = anndata.AnnData(
     X = np.zeros((predictions_metadata.shape[0], train.n_vars)),
@@ -66,15 +81,10 @@ for _, current_prediction_metadata in get_unique_rows(predictions.obs, ['perturb
         (predictions.obs["perturbation"]==current_prediction_metadata["perturbation"]) & \
         (predictions.obs["expression_level_after_perturbation"]==current_prediction_metadata["expression_level_after_perturbation"]) & \
         (predictions.obs["prediction_timescale"]==current_prediction_metadata["prediction_timescale"]) 
-    # try:
     train_index = (train.obs["cell_type"]==current_prediction_metadata["cell_type"]) & (train.obs["timepoint"]==current_prediction_metadata["timepoint"])
     goi, level, prediction_timescale = current_prediction_metadata[['perturbation', "expression_level_after_perturbation", 'prediction_timescale']]
     for i in np.where(prediction_index)[0]:
-        predictions[i, :].X = predict_one(goi, level, current_prediction_metadata["cell_type"], current_prediction_metadata["timepoint"], prediction_timescale)
-    # except ValueError as e:
-        # predictions[prediction_index, :].X = np.nan
-        # print("Prediction failed. Error text: " + str(e))
-
+        predictions[i, :].X = predict_many(kwargs["num_cells_to_simulate"], goi, level, current_prediction_metadata["cell_type"], current_prediction_metadata["timepoint"], prediction_timescale) 
     
 print("Saving results.")
 predictions.write_h5ad("from_to_docker/predictions.h5ad")

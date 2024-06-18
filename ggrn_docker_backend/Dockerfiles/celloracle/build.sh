f3285b01677dd0d3243bc2bb732b26eb5bcf38d0
Eric Kernfeld <eric.kern13@gmail.com>
2024-06-18 11:53:03 -0400

    Drastically simplify celloracle dockerfile and successfully test calling CO via ggrn

--- ggrn_docker_backend/Dockerfiles/celloracle/build.sh
+++ ggrn_docker_backend/Dockerfiles/celloracle/build.sh
@@ -2,6 +2,10 @@
 # Boilerplate to build this docker image
 
 docker build -t ggrn_docker_backend_celloracle .
+
+# We use this for easy interactive development but it should usually be commented out. 
+# docker run --rm -it -v /home/ekernf01/Desktop/jhu/research/projects/perturbation_prediction/cell_type_knowledge_transfer/ggrn/ggrn_docker_backend/Dockerfiles/celloracle/from_to_docker:/from_to_docker ggrn_docker_backend_celloracle
+
 docker tag ggrn_docker_backend_celloracle ekernf01/ggrn_docker_backend_celloracle
 docker login
 docker push ekernf01/ggrn_docker_backend_celloracle

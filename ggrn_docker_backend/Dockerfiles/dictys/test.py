337a21e2be231051416ee97b25241dbd43565939
Eric Kernfeld <eric.kern13@gmail.com>
2024-09-17 18:02:19 -0400

    Impose dictys minimum expression cutoff separately in each cell type. Log errors in predictions.obs['error_message'].

--- ggrn_docker_backend/Dockerfiles/dictys/test.py
+++ ggrn_docker_backend/Dockerfiles/dictys/test.py
@@ -6,12 +6,12 @@ import pereggrn_networks
 import pandas as pd
 pereggrn_networks.set_grn_location("../../../../network_collection/networks") # you may need to change this to the path where the networks are stored on your computer.
 pereggrn_perturbations.set_data_path("../../../../perturbation_data/perturbations") # you may need to change this to the path where the perturbations are stored on your computer.
-train = pereggrn_perturbations.load_perturbation("definitive_endoderm", is_timeseries=True)
-test = pereggrn_perturbations.load_perturbation("definitive_endoderm", is_timeseries=False)
+train = pereggrn_perturbations.load_perturbation("saunders_endoderm", is_timeseries=True)
+test = pereggrn_perturbations.load_perturbation("saunders_endoderm", is_timeseries=False)
 top_genes = train.var.query("highly_variable_rank <= 2000").index
 train = train[:, list(set(list(test.uns["perturbed_and_measured_genes"]) + list(top_genes)))]
 test  =  test[:, list(set(list(test.uns["perturbed_and_measured_genes"]) + list(top_genes)))]
-grn = ggrn.GRN(train,  network=pereggrn_networks.LightNetwork("celloracle_human"))
+grn = ggrn.GRN(train,  network=pereggrn_networks.LightNetwork("celloracle_zebrafish"))
 grn.fit(
     method = "docker____ekernf01/ggrn_docker_backend_dictys", 
     # These args are suitable for fast debugging but nstep=4000 is the default for actual use
@@ -38,3 +38,4 @@ predictions_metadata = pd.merge(
                     how = "cross", 
                 )
 ad_out = grn.predict(predictions_metadata = predictions_metadata.loc[np.random.choice(predictions_metadata.index, 50),:])
+print(ad_out.obs)
--- ggrn_docker_backend/Dockerfiles/dictys/train.py
+++ ggrn_docker_backend/Dockerfiles/dictys/train.py
@@ -30,12 +30,92 @@ def make_unique(original_list):
         unique_list.append(new_item)
     return unique_list
 
+
+def filter_minimum_expression(train):
+    # Dictys requires a minimum expression cutoff.
+    print("Filtering genes for minimum expression.")
+    ise = train.X.sum(axis = 0) >= kwargs['minimum_expression']*train.X.shape[0]
+    train.var["is_sufficiently_expressed"] = ise.T
+    train = train[:, train.var["is_sufficiently_expressed"]]
+    return train
+
+
+def convert_network_format(train, network):
+    print("Converting inputs to dictys' preferred format.", flush=True)
+    # Intersect the network with the expression data
+    # If there is a regulator in the network that does not appear as a target in the network (has no incoming edges),
+    # This causes a pattern of zeros that dictys.network.reconstruct cannot handle.
+    # So we remove any regulators that are not also targets, focusing on targets that are also in the expression data.
+    network_features = set(network.get_all_one_field("target")).union(set(network.get_all_one_field("regulator")))
+    common_features = list(train.var.index.intersection(network_features))
+    network = network.get_all().query("regulator in @common_features and target in @common_features")
+    train = train[:, common_features]
+    # Convert to dictys' preferred format via celloracle's preferred format
+    network = pivotNetworkLongToWide(network) 
+    network.index = network["gene_short_name"]
+    del network["gene_short_name"]
+    del network["peak_id"]
+    # Input network must be square. Make it square by zero-padding:
+    #   - specify no targets for any non-TF.
+    #   - If a gene is listed as a TF but not a target, add it to the targets.
+    network_non_regulators = list(set(train.var.index) - set(network.columns))
+    network.loc[:,network_non_regulators] = 0
+    network_non_targets = list(set(train.var.index) - set(network.index))
+    network = pd.concat([network, pd.DataFrame({tf:0 for tf in network.columns}, index = network_non_targets)])
+    network = network.loc[common_features, common_features]
+    # Remove self loops. Without this, we trigger an assertion error in dictys reconstruct line 790, which is documented behavior.
+    for g in network.columns:
+        network.loc[g,g]=0
+
+    if any(pd.isnull(network).any()) or \
+        any(common_features!=network.columns.astype("str")) or \
+            any(common_features!=network.index.values.astype("str") ):
+        print(set(common_features).difference(network.columns))
+        print(set(common_features).difference(network.index))
+        print(set(network.index).difference(common_features))
+        print(set(network.columns).difference(common_features))
+        raise ValueError("There are NaNs or incorrect genes or missing genes in the input network. This will cause dictys to fail.")
+    return network, common_features
+
+def augment_data(X, desired_total):
+    """Add resampled dummy cells to the data to make it square
+
+    Args:
+        X (pd.DataFrame): expression data
+
+    Returns:
+        pd.DataFrame: expression data with resampled dummy cells added
+    """
+    num_new_cells = desired_total - X.shape[0]
+    average_expression = X.sum(axis = 0)
+    newX = pd.DataFrame(index = range(num_new_cells), columns = X.columns)
+    for i in range(num_new_cells):
+        newX.loc[i, :] = [np.random.binomial(np.random.poisson(x), 1/X.shape[0]) for x in average_expression]
+    return pd.concat([X, newX])
+
+def construct_inputs(train, network, celltype):
+    cells_to_use = train.obs.query("cell_type==@ct").index
+    X = filter_minimum_expression(train[cells_to_use, :].copy())
+    current_network, common_features = convert_network_format(X, network.copy())
+    train.raw.var.index = make_unique(train.raw.var.index)
+    try:
+        X = pd.DataFrame(train.raw[cells_to_use, common_features].X.toarray(), index = make_unique(cells_to_use), columns = common_features)
+    except AttributeError:
+        X = pd.DataFrame(train.raw[cells_to_use, common_features].X,           index = make_unique(cells_to_use), columns = common_features)
+    # dictys.network.reconstruct errs unless n>p, so we add dummy cells in that case. 
+    if X.shape[0] < current_network.shape[0]: 
+        print("Augmenting data since we have fewer cells than genes.")
+        X = augment_data(X.copy(), current_network.shape[0])
+    return X, current_network
+
+
 print("Training a model via dictys.", flush=True)
+print("Reading data...")
 os.chdir("from_to_docker")
 train = sc.read_h5ad("train.h5ad")
 predictions_metadata = pd.read_csv("predictions_metadata.csv")
 for c in ['timepoint', 'cell_type', 'perturbation', "expression_level_after_perturbation", 'prediction_timescale']:
-    assert c in predictions_metadata.columns, f"For the ggrn prescient backend, predictions_metadata.columns is required to contain {c}."
+    assert c in predictions_metadata.columns, f"For the ggrn dictys backend, predictions_metadata.columns is required to contain {c}."
 
 with open(os.path.join("kwargs.json")) as f:
     kwargs = json.load(f)
@@ -44,15 +124,15 @@ with open(os.path.join("ggrn_args.json")) as f:
     ggrn_args = json.load(f)
 
 try:
-    network = LightNetwork(files = ["network.parquet"])
+    network = LightNetwork(files = [f"{os.getcwd()}/network.parquet"])
 except FileNotFoundError as e:
     raise ValueError(
 """
 Could not read in the network (from_to_docker/network.parquet). GGRN cannot use the dictys backend without a user-provided base network. 
-Base networks are usually derived from footprint analysis of cell-type-specific ATAC data. Many similar networks are available in out collection. 
+Dicys base networks are usually derived from footprint analysis of cell-type-specific ATAC data. Many similar networks are available in out collection. 
 You have two options: 
    - If you are using GGRN directly, construct a LightNetwork object using pereggrn_networks.LightNetwork() and pass it to the ggrn.api.GRN constructor.
-   - If you are using GGRN indirectly via PEREGGRN, add a network to your experiment's metadata.json.
+   - If you are using GGRN indirectly via PEREGGRN, add a name of a network to your experiment's metadata.json.
 More information should be available in the documentation for GGRN and for PEREGGRN.
 """
 )
@@ -80,87 +160,17 @@ for k in defaults:
     if k not in kwargs:
         kwargs[k] = defaults[k]
 
-# Dictys requires a minimum expression cutoff.
-print("Filtering genes for minimum expression.")
-ise = train.X.sum(axis = 0) >= kwargs['minimum_expression']*train.X.shape[0]
-train.var["is_sufficiently_expressed"] = ise.T
-for g in predictions_metadata["perturbation"]:
-    if g in train.var_names and not train.var.loc[g, "is_sufficiently_expressed"]:
-        print(f"Gene {g} falls below minimum_expression ({kwargs['minimum_expression']}). Perturbations of it will not be predicted.")
-        predictions_metadata = predictions_metadata.query("perturbation!=@g")
-train_all_features = train.copy()
-train = train[:, train.var["is_sufficiently_expressed"]]
-
-print("Converting inputs to dictys' preferred format.", flush=True)
-# Intersect the network with the expression data
-# If there is a regulator in the network that does not appear as a target in the network (has no incoming edges),
-# This causes a pattern of zeros that dictys.network.reconstruct cannot handle.
-# So we remove any regulators that are not also targets, focusing on targets that are also in the expression data.
-network_features = set(network.get_all_one_field("target")).union(set(network.get_all_one_field("regulator")))
-common_features = list(train.var.index.intersection(network_features))
-common_features_int = np.array([train.var.index.get_loc(f) for f in common_features])
-network = network.get_all().query("regulator in @common_features and target in @common_features")
-train = train[:, common_features]
-# Convert to dictys' preferred format via celloracle's preferred format
-network = pivotNetworkLongToWide(network) 
-network.index = network["gene_short_name"]
-del network["gene_short_name"]
-del network["peak_id"]
-# Input network must be square. Make it square by zero-padding:
-#   - specify no targets for any non-TF.
-#   - If a gene is listed as a TF but not a target, add it to the targets.
-network_non_regulators = list(set(train.var.index) - set(network.columns))
-network.loc[:,network_non_regulators] = 0
-network_non_targets = list(set(train.var.index) - set(network.index))
-network = pd.concat([network, pd.DataFrame({tf:0 for tf in network.columns}, index = network_non_targets)])
-network = network.loc[common_features, common_features]
-# Remove self loops. Without this, we trigger an assertion error in dictys reconstruct line 790, which is documented behavior.
-for g in network.columns:
-    network.loc[g,g]=0
-
-if any(pd.isnull(network).any()) or \
-    any(common_features!=network.columns.astype("str")) or \
-        any(common_features!=network.index.values.astype("str") ):
-    print(set(common_features).difference(network.columns))
-    print(set(common_features).difference(network.index))
-    print(set(network.index).difference(common_features))
-    print(set(network.columns).difference(common_features))
-    raise ValueError("There are NaNs or incorrect genes or missing genes in the input network. This will cause dictys to fail.")
-
-def augment_data(X, desired_total):
-    """Add resampled dummy cells to the data to make it square
-
-    Args:
-        X (pd.DataFrame): expression data
-
-    Returns:
-        pd.DataFrame: expression data with resampled dummy cells added
-    """
-    num_new_cells = desired_total - X.shape[0]
-    average_expression = X.sum(axis = 0)
-    newX = pd.DataFrame(index = range(num_new_cells), columns = X.columns)
-    for i in range(num_new_cells):
-        newX.loc[i, :] = [np.random.binomial(np.random.poisson(x), 1/X.shape[0]) for x in average_expression]
-    return pd.concat([X, newX])
-
 indirect_effects = dict()
 for ct in train.obs["cell_type"].unique():
+    print(f"Computing direct and indirect effects for cell type {ct}...")
     os.makedirs(ct, exist_ok=True)
     with temporaryWorkingDirectory(ct):
         # exporttttt
-        cells_to_use = train.obs.query("cell_type==@ct").index        
-        try:
-            X = pd.DataFrame(train.raw[cells_to_use, common_features].X.toarray(), index = make_unique(cells_to_use), columns = common_features)
-        except AttributeError:
-            X = pd.DataFrame(train.raw[cells_to_use, common_features].X,           index = make_unique(cells_to_use), columns = common_features)
+        current_X, current_network = construct_inputs(train, network, ct)
         # Dictys expects one TF per row and CO uses one per column. We transpose the network to match the expected format.
-        network.T.to_csv("mask.tsv", sep="\t")
-        # dictys.network.reconstruct errs unless n>p, so we add dummy cells in that case. 
-        if X.shape[0] < network.shape[0]: 
-            print("Augmenting data since we have fewer cells than genes.")
-            X = augment_data(X, network.shape[0])
+        current_network.T.to_csv("mask.tsv", sep="\t")
         # Dictys expects one gene per row, one cell per column, unlike typical ML or stats data tables. So, transpose before saving.
-        X.T.to_csv("exp.tsv", sep="\t")
+        current_X.T.to_csv("exp.tsv", sep="\t")
         print("Running network reconstruction.", flush=True)
         dictys.network.reconstruct(
             fi_exp="exp.tsv",
@@ -197,12 +207,12 @@ for ct in train.obs["cell_type"].unique():
         )
         indirect_effects[ct] = pd.read_csv("iweight.tsv", sep="\t", index_col=0)
 
-def predict_expression(perturbed_gene: str, level: float, cell_type: str, timepoint: int):
+def predict_expression(perturbed_genes: str, levels: float, cell_type: str, timepoint: int):
     """Predict log-scale post-perturbation expression
 
     Args:
-        perturbed_gene (str): gene of interest, the perturbation
-        level (float): the expression level of perturbed_gene after the perturbation
+        perturbed_genes (str): gene or genes perturbed; comma-separated like "OCT4,SOX2"
+        levels (float): the expression level of perturbed_gene after the perturbation, comma-separated like "1.5,2.0"
         cell_type (str): starting cell type to perturb
         timepoint (int): starting timepoint to perturb
 
@@ -210,34 +220,45 @@ def predict_expression(perturbed_gene: str, level: float, cell_type: str, timepo
         np.array: the predicted log-scale expression of all genes. For genes not in the dictys model, returns control expression.
     """
     is_baseline = (
-        (train.obs["cell_type"]==current_prediction_metadata["cell_type"]) &
-        (train.obs["timepoint"]==current_prediction_metadata["timepoint"]) &
+        (train.obs["cell_type"]==cell_type) &
+        (train.obs["timepoint"]==timepoint) &
         train.obs["is_control"]
     )
     if not any(is_baseline):
-        raise ValueError(f"No controls were found at time {current_prediction_metadata['timepoint']} for celltype {current_prediction_metadata['cell_type']}. Cannot predict perturbed expression.")
-    control_expression = train_all_features[is_baseline, :].X.mean(axis = 0)
+        raise ValueError(f"No controls were found at time {timepoint} for celltype {cell_type}. Cannot predict perturbed expression.")
+    control_expression = train[is_baseline, :].X.mean(axis = 0)
     try: 
         control_expression = control_expression.A1
     except AttributeError:
         pass
-    if perturbed_gene in train.var.index:
-        change_in_perturbed_gene = level - train[is_baseline, perturbed_gene].X.mean()
-        logfc = indirect_effects[cell_type].loc[perturbed_gene, :]*change_in_perturbed_gene  # Index name is "regulator" so we want to grab a row, not a column
-        logfc = logfc.values
-    else:
-        logfc = 0
-    control_expression[common_features_int] = control_expression[common_features_int] + logfc
-    return control_expression
+    for perturbed_gene, level in zip(perturbed_genes.split(","), [float(x) for x in str(levels).split(",")]):
+        if not perturbed_gene in network.get_all_regulators():   
+            logfc = 0
+            error_message = f"{perturbed_gene} is not in the input network."
+        elif not perturbed_gene in train.var.index:
+            logfc = 0
+            error_message = f"{perturbed_gene} is not measured in the training data."
+        elif not (perturbed_gene in indirect_effects[cell_type].index):
+            logfc = 0
+            error_message = f"{perturbed_gene} is not in the final model for cell type {cell_type}."
+        else:
+            change_in_perturbed_gene = level - train[is_baseline, perturbed_gene].X.mean()
+            logfc = indirect_effects[cell_type].loc[perturbed_gene, :]*change_in_perturbed_gene  #  in the indirect effects matrix, each row is one regulator
+            logfc = logfc.values
+            error_message = ""
+        original_indices_of_current_features = np.array([train.var.index.get_loc(f) for f in indirect_effects[cell_type].columns]) # in the indirect effects matrix, each column is one target 
+        control_expression[original_indices_of_current_features] = control_expression[original_indices_of_current_features] + logfc
+    return control_expression, error_message
 
 
-
-print("Running simulations")
+print("Computing predictions...")
 predictions = anndata.AnnData(
-    X = np.zeros((predictions_metadata.shape[0], train_all_features.n_vars)),
+    X = np.zeros((predictions_metadata.shape[0], train.n_vars)),
     obs = predictions_metadata,
-    var = train_all_features.var,
+    var = train.var,
 )
+predictions.obs["error_message"] = ""
+
 for _, current_prediction_metadata in predictions.obs[['perturbation', "expression_level_after_perturbation", 'prediction_timescale', 'timepoint', 'cell_type']].drop_duplicates().iterrows():
     prediction_index = \
         (predictions.obs["cell_type"]==current_prediction_metadata["cell_type"]) & \
@@ -245,12 +266,13 @@ for _, current_prediction_metadata in predictions.obs[['perturbation', "expressi
         (predictions.obs["perturbation"]==current_prediction_metadata["perturbation"]) & \
         (predictions.obs["expression_level_after_perturbation"]==current_prediction_metadata["expression_level_after_perturbation"]) & \
         (predictions.obs["prediction_timescale"]==current_prediction_metadata["prediction_timescale"]) 
-    x = predict_expression(
-        perturbed_gene=current_prediction_metadata["perturbation"], 
-        level = current_prediction_metadata["expression_level_after_perturbation"], 
+    x, error_message = predict_expression(
+        perturbed_genes=current_prediction_metadata["perturbation"], 
+        levels = current_prediction_metadata["expression_level_after_perturbation"], 
         cell_type = current_prediction_metadata["cell_type"],
         timepoint = current_prediction_metadata["timepoint"]
     )
+    predictions.obs.loc[prediction_index, "error_message"] = error_message
     for i in np.where(prediction_index)[0]:
         predictions[i, :].X = x
     

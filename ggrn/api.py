c62613b69206ea176f97aeda6a92b20727886246
Eric Kernfeld <eric.kern13@gmail.com>
2024-09-18 17:42:37 -0400

    Fix a futurewarning in control matching

--- ggrn/api.py
+++ ggrn/api.py
@@ -1445,5 +1445,7 @@ def match_controls(train_data: anndata.AnnData, matching_method: str, matched_co
     # The above code provides an integer index for each matched control. This code is for if you want e.g. a cell barcode instead.
     if not matched_control_is_integer and matching_method.lower() != "user":
         has_matched_control = train_data.obs["matched_control"].notnull()
+        train_data.obs.loc[~has_matched_control, "matched_control"] = ""
         train_data.obs.loc[has_matched_control, "matched_control"] = train_data.obs.index[train_data.obs.loc[has_matched_control, "matched_control"]]
+        train_data.obs.loc[~has_matched_control, "matched_control"] = np.nan
     return train_data

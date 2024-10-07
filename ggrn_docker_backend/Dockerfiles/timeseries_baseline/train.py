3384b8fff6e3474a2332b9f7c1417bc92224afe0
Eric Kernfeld <eric.kern13@gmail.com>
2024-10-07 14:53:23 -0400

    Simplify prediction code so that controls do not get overlooked

--- ggrn_docker_backend/Dockerfiles/timeseries_baseline/train.py
+++ ggrn_docker_backend/Dockerfiles/timeseries_baseline/train.py
@@ -4,11 +4,9 @@ import os
 import numpy as np
 import pandas as pd
 import scanpy as sc
-import sys
-sys.path.append(".")
 
+print("Reading inputs.", flush=True)
 train = sc.read_h5ad("from_to_docker/train.h5ad")
-
 predictions_metadata = pd.read_csv("from_to_docker/predictions_metadata.csv")
 for c in ['timepoint', 'cell_type', 'perturbation', "expression_level_after_perturbation", 'prediction_timescale']:
     assert c in predictions_metadata.columns, f"For the timeseries baseline that returns the unperturbed timeseries, predictions_metadata.columns is required to contain {c}."
@@ -30,11 +28,12 @@ for k in defaults:
     if k not in kwargs:
         kwargs[k] = defaults[k]
 
+print("Calculating cell type means.", flush=True)
 cell_type_means = {
     ct: np.array(train[train.obs["cell_type"]==ct, :].X.mean(axis = 0))
     for ct in train.obs["cell_type"].unique()
 }
-def predict_many(tf: str, expression_level_after_perturbation: float, cell_type: str, timepoint: int, num_steps: int):
+def predict_many(cell_type: str, timepoint: int, num_steps: int):
     """Predict expression after TF perturbation
 
     Args:
@@ -52,28 +51,34 @@ def predict_many(tf: str, expression_level_after_perturbation: float, cell_type:
         next_cell_type = train.obs.loc[next_cells, "cell_type"].value_counts().idxmax()
         return cell_type_means[next_cell_type]
 
-print("Running simulations")
+print("Running simulations", flush=True)
 predictions = anndata.AnnData(
     X = np.zeros((predictions_metadata.shape[0], train.n_vars)),
     obs = predictions_metadata,
     var = train.var,
 )
-predictions.obs["error_message"] = ""
-for _, current_prediction_metadata in predictions.obs[['perturbation', "expression_level_after_perturbation", 'prediction_timescale', 'timepoint', 'cell_type']].drop_duplicates().iterrows():
+predictions.obs["error_message"] = "no predictions made due to an error in the Docker backend." # This should not appear.
+num_cells_predicted = 0
+for _, current_prediction_metadata in predictions.obs[['prediction_timescale', 'timepoint', 'cell_type']].drop_duplicates().iterrows():
+    print(".", end="", flush=True)
+    num_cells_predicted +=1
+    if num_cells_predicted == 100:
+        print("", flush=True)
+        num_cells_predicted = 0
     prediction_index = \
         (predictions.obs["cell_type"]==current_prediction_metadata["cell_type"]) & \
         (predictions.obs["timepoint"]==current_prediction_metadata["timepoint"]) & \
-        (predictions.obs["perturbation"]==current_prediction_metadata["perturbation"]) & \
-        (predictions.obs["expression_level_after_perturbation"]==current_prediction_metadata["expression_level_after_perturbation"]) & \
         (predictions.obs["prediction_timescale"]==current_prediction_metadata["prediction_timescale"]) 
     train_index = (train.obs["cell_type"]==current_prediction_metadata["cell_type"]) & (train.obs["timepoint"]==current_prediction_metadata["timepoint"])
-    goi, level, prediction_timescale = current_prediction_metadata[['perturbation', "expression_level_after_perturbation", 'prediction_timescale']]
+    try:
+        new_prediction = predict_many(current_prediction_metadata["cell_type"], current_prediction_metadata["timepoint"], current_prediction_metadata['prediction_timescale']) 
+        error_message = ""
+    except Exception as e:
+        new_prediction = np.nan
+        error_message = repr(e)
     for i in prediction_index[prediction_index].index:
-        try:
-            predictions[i, :].X = predict_many(goi, level, current_prediction_metadata["cell_type"], current_prediction_metadata["timepoint"], prediction_timescale) 
-        except Exception as e:
-            predictions.obs.loc[i, "error_message"] = repr(e)
-            predictions[i, :].X = np.nan
+            predictions.obs.loc[i, "error_message"] = error_message
+            predictions[i, :].X = new_prediction
 
 print("Summary of errors:")
 print(predictions.obs["error_message"].value_counts())
